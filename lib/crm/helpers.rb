fa016484fdc1b03b5534347d3d1d1a36747da4b7
Huy Dinh <mail@huydinh.eu>
2016-05-21 17:04:00 +0200

    Provide validators for each CRM attribute type.

--- lib/crm/helpers.rb
+++ lib/crm/helpers.rb
@@ -2,13 +2,20 @@ require 'active_model'
 require 'active_support/all'
 require 'infopark_webcrm_sdk'
 
+require 'crm/helpers/validators/crm_attribute_validator_helper'
+
 require 'crm/helpers/validators/crm_attribute_validator'
+require 'crm/helpers/validators/crm_boolean_validator'
 require 'crm/helpers/validators/crm_datetime_validator'
+require 'crm/helpers/validators/crm_enum_validator'
+require 'crm/helpers/validators/crm_integer_validator'
 require 'crm/helpers/validators/crm_list_validator'
 require 'crm/helpers/validators/crm_multienum_validator'
+require 'crm/helpers/validators/crm_multienum_validator'
+require 'crm/helpers/validators/crm_string_validator'
+require 'crm/helpers/validators/crm_text_validator'
 
 require 'crm/helpers/attributes'
-require 'crm/helpers/validations'
 require 'crm/helpers/version'
 
 require 'crm/helper'
--- lib/crm/helpers/validators/crm_datetime_validator.rb
+++ lib/crm/helpers/validators/crm_datetime_validator.rb
@@ -2,6 +2,8 @@ module Crm
   module Helpers
     module Validators
       class CrmDatetimeValidator < ActiveModel::EachValidator
+        include Crm::Helpers::Validators::CrmAttributeValidatorHelper
+
         def validate_each(record, attribute, value)
           return if value.is_a?(Date)
           return if value.is_a?(Time)
