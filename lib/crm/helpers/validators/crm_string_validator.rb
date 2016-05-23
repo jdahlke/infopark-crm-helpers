f44a791909ed7733ed2d77c09fa54ee204f84b3e
Huy Dinh <mail@huydinh.eu>
2016-05-23 15:27:17 +0200

    Don't validate for string/text length, if no maximum length is given.

--- lib/crm/helpers/validators/crm_string_validator.rb
+++ lib/crm/helpers/validators/crm_string_validator.rb
@@ -4,6 +4,8 @@ module Crm
       class CrmStringValidator < CrmEachValidator
         def validate_each(record, attribute, _)
           definition = crm_attribute_definition(record, attribute)
+          return if definition['max_length'].blank?
+
           record.validates_length_of attribute, maximum: definition['max_length']
         end
       end
