382d57efe89358f7f684201f7df3455b9525bcb5
Huy Dinh <mail@huydinh.eu>
2016-05-31 17:39:03 +0200

    Refactor the CrmAttributeValidatorSpecHelper for less code duplication.

--- spec/support/crm_attribute_validator_spec_helper.rb
+++ spec/support/crm_attribute_validator_spec_helper.rb
@@ -38,21 +38,32 @@ module CrmAttributeValidatorSpecHelper
   end
 
   def run_specs_for_invalid_values(invalid_values)
-    invalid_values.each do |invalid_value|
-      it 'should successfully validate and add an error message' do
-        subject.attribute = invalid_value
-        expect(subject).to_not be_valid
-        expect(subject.errors[:attribute]).to_not be_empty
+    check_for_validity(invalid_values, false)
+    check_for_empty_error_messages(invalid_values, false)
+  end
+
+  def run_specs_for_valid_values(valid_values)
+    check_for_validity(valid_values, true)
+    check_for_empty_error_messages(valid_values, true)
+  end
+
+  protected
+
+  def check_for_validity(values, expectation)
+    values.each do |value|
+      it "should be #{expectation ? 'valid' : 'invalid'}" do
+        subject.attribute = value
+        expect(subject.valid?).to eq(expectation)
       end
     end
   end
 
-  def run_specs_for_valid_values(valid_values)
-    valid_values.each do |valid_value|
-      it 'should successfully validate and not add errors' do
-        subject.attribute = valid_value
-        expect(subject).to be_valid
-        expect(subject.errors[:attribute]).to be_empty
+  def check_for_empty_error_messages(values, expectation)
+    values.each do |value|
+      it "#{expectation ? 'should not' : 'should'} add error messages" do
+        subject.attribute = value
+        subject.valid?
+        expect(subject.errors[:attribute].empty?).to eq(expectation)
       end
     end
   end
