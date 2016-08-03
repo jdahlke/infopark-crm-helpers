846409866442e1f631e111e6630fc85a8cb27b1c
Joergen Dahlke <joergen.dahlke@infopark.de>
2016-08-03 16:02:39 +0200

    implement #find with array of ids

--- lib/crm/helpers/finders.rb
+++ lib/crm/helpers/finders.rb
@@ -5,11 +5,20 @@ module Crm
         base.extend self
       end
 
-      def find(id)
-        crm_object = crm_class.find(id)
-        return nil if crm_object.blank?
+      def find(id_or_ids)
+        case id_or_ids
+        when String
+          crm_object = crm_class.find(id_or_ids)
+          return nil if crm_object.blank?
 
-        new(crm_object.attributes)
+          new(crm_object.attributes)
+        when Array
+          crm_objects = Crm.find(id_or_ids).to_a
+          rejected_crm_objects = crm_objects.reject { |crm_object| crm_object.is_a?(crm_class) }
+          raise Errors::ResourceNotFound.new("Not of type #{crm_class.name}", rejected_crm_objects.map(&:id)) if rejected_crm_objects.present?
+
+          crm_objects.map { |crm_object| new(crm_object.attributes) }
+        end
       end
 
       def find_by_query(query, options = {})
